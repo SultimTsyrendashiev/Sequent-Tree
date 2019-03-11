d9058f30ff9bed18f2f7e195ec91665be981007e
SultimTsyrendashiev <sultim222@mail.ru>
2019-03-11 12:19:24 +0300

    - name fixes

--- Sequent.hs
+++ Sequent.hs
@@ -1,8 +1,9 @@
 module SequentTree (
   Expr,
   SeqTree,
-  solveSeq,
-  solve) where
+  createTree,
+  hasCE,
+  isValid) where
 
 data Expr = Var String
           | Not Expr
@@ -39,25 +40,30 @@ data SeqTree = Empty
 instance Show SeqTree where
   show Empty = ""
   show (Node (xs, ys) [Empty]) = show xs ++ " |- " ++ show ys
-  show (Node (xs, ys) tr) = show xs ++ " |- " ++ show ys ++ "\n-----------\n" ++ concatMap showSpaced tr
+  show (Node (xs, ys) tr) = concatMap showSpaced tr ++ "\n-----------\n" ++ show xs ++ " |- " ++ show ys
 
--- Show with spaces after element
+-- Show with spaces after element --
 showSpaced :: Show a => a -> String
 showSpaced a = show a ++ "       "
 
--- Solve expression --
-solve :: Expr -> Maybe SeqTree
-solve e = let tree = solveSeq ([], [e])
-          in if isValid tree
-             then Just tree
-             else Nothing
+-- Check sequent for validness --
+isValid :: Sequent -> Maybe SeqTree
+isValid s = let (has, tree) = hasCE s
+            in if has
+               then Just tree
+               else Nothing
 
--- Check validness of a tree --
-isValid :: SeqTree -> Bool
-isValid Empty = True
+-- Check sequent for counter examples --
+hasCE :: Sequent -> (Bool, SeqTree)
+hasCE s = let tree = createTree s
+          in (treeHasCE tree, tree)
+
+-- Check tree for counter examples --
+treeHasCE :: SeqTree -> Bool
+treeHasCE Empty = True
 -- if node is a leaf
-isValid (Node (xs,ys) [Empty]) = not $ haveSame xs ys
-isValid (Node _ tree) = foldr ((&&) . isValid) True tree
+treeHasCE (Node (xs,ys) [Empty]) = not $ haveSame xs ys -- if there are no same vars on both sides => CE exist
+treeHasCE (Node _ tree) = foldr ((&&) . treeHasCE) True tree
 
 haveSame :: [Expr] -> [Expr] -> Bool
 haveSame [] _ = False
@@ -65,12 +71,12 @@ haveSame (x:xs) ys | x `elem` ys = True
                    | otherwise   = haveSame xs ys
 
 -- Create sequent tree --
-solveSeq :: Sequent -> SeqTree
-solveSeq ([], []) = Empty
-solveSeq s = let list = procSeq s
+createTree :: Sequent -> SeqTree
+createTree ([], []) = Empty
+createTree s = let list = procSeq s
           in if null list
              then Node s [Empty]
-             else Node s $ map solveSeq list
+             else Node s $ map createTree list
 
 -- Returns [] if all members are variables
 procSeq :: Sequent -> [Sequent]
